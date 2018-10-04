c836bd09ac0ca2212253503f361eac62c339c0ed
Daniel <hoptond848@protonmail.com>
2018-10-04 17:06:05 +0100

    added dockblocks where necessary

--- edit/login.php
+++ edit/login.php
@@ -1,15 +1,15 @@
 <?php require 'security_functions.php';
 
 session_start();
-if(verifyLogin($_POST['username'], $_POST['password'])) {
+if (verifyLogin($_POST['username'], $_POST['password'])) {
     setcookie('id', session_id());
     header('Location: dash.php');
 } else {
     session_start();
-    if(!loginExists($_POST['username'])) {
+    if (!loginExists($_POST['username'])) {
         echo 'adding badname to $_SESSION' . '<br>';
         $_SESSION['badname'] = TRUE;
-    } else if(!correctPassword($_POST['password'], $_POST['password'])) {
+    } else if (!correctPassword($_POST['password'], $_POST['password'])) {
         echo 'adding badpass to $_SESSION' . '<br>';
         $_SESSION['badpass'] = TRUE;
     }
--- edit/security_functions.php
+++ edit/security_functions.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * Verifies the username and password. If it is correct, any session data relating to incorrect entry is removed.
+ *
+ * @param string $username The given username.
+ * @param string $password The given password.
+ *
+ * @return bool Returns TRUE if the credentials match, otherwise FALSE.
+ */
 function verifyLogin($username, $password) : bool {
     session_start();
-    if(loginExists($username)) {
-        if(correctPassword($username, $password)) {
+    if (loginExists($username)) {
+        if (correctPassword($username, $password)) {
             unset($_SESSION['badname']);
             unset($_SESSION['badpass']);
             return true;
@@ -12,22 +20,31 @@ function verifyLogin($username, $password) : bool {
     return false;
 }
 
+/*
+ * If the cookie ID does not match the session ID, the user is punted out of the session. GET OUTTA HERE
+ */
 function verifyUser() {
     session_start();
-    if($_COOKIE['id'] != session_id()) {
+    if ($_COOKIE['id'] != session_id()) {
         setcookie('id',0, time() - 1);
         header('Location: index.php');
     }
     session_destroy();
 }
 
+/*
+ * Verifies the username and password. If it is correct, any session data relating to incorrect entry is removed.
+ *
+ * @return string Returns a message if either the username or password is incorrect. If the user has entered no data,
+ * this value is blank.
+ */
 function loginFailMessage() : string {
     session_start();
-    if(array_key_exists('badname' , $_SESSION)) {
+    if (array_key_exists('badname' , $_SESSION)) {
         session_destroy();
         return 'User not in database. Please try again';
     }
-    if(array_key_exists('badpass', $_SESSION)) {
+    if (array_key_exists('badpass', $_SESSION)) {
         session_destroy();
         return 'Wrong password. Please try again.';
     }
@@ -35,17 +52,32 @@ function loginFailMessage() : string {
 }
 
 
-
+/*
+ * Determines if the login exists. At the moment, we only have a static login, so it is compared against that.
+ *
+ * @param string $username The given username.
+ *
+ * @return bool Returns TRUE if the username matches, otherwise false.
+ */
 function loginExists($username) : bool {
-    if($username == 'badmin') {
+    if ($username == 'badmin') {
         return true;
     }
     return false;
 }
 
-function correctPassword($username ,$password) {
+/*
+ * Determines if the password is correct. At the moment, we only have a static password hash, so it is compared against that.
+ *
+ * @param string $username The given username. At the moment, this value does nothing because we only have a static entry but
+ * in a real DB this would have to be compared in a query.
+ * @param string $password The given password.
+ *
+ * @return bool Returns TRUE if the password matches, otherwise false.
+ */
+function correctPassword($username, $password) {
     $dbpassword = '$2y$10$4u0SlOo/oAhRLwaMuC17BOWCjF/YUT2TJqGh4F4h8dnurxVJuxH2K';
-    if(password_verify($password, $dbpassword)) {
+    if (password_verify($password, $dbpassword)) {
         return true;
     }
     return false;
