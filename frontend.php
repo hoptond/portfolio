6bd390cf0cec9952cf2a67f3854b1f1b31f4a367
Daniel <hoptond848@protonmail.com>
2018-11-19 16:32:27 +0000

    handle display of projects where there is either a git link, a project link, or both

--- frontend.php
+++ frontend.php
@@ -84,18 +84,30 @@ function getProjectImages(array $projects) {
 function getProjectLinks(array $projects) {
     $output = '';
     for ($i = 0; $i < count($projects); $i++) {
-        if($i === 0) {
-            $output .= '<div class="showcasebottom">
-                            <a class="showcaseview" href="' . $projects[$i]['repo_link'] . '">View Project</a>
+        $output .= '<div class="showcasebottom hidden">
+                            <span>' . getProjectLinkSpanContent($projects[$i]) .'</span>
                         </div>';
-        } else {
-            $output .= '<div class="showcasebottom hidden">
-                            <a class="showcaseview" href="' . $projects[$i]['repo_link'] . '">View Project</a>
-                        </div>';
-        }
     }
     return $output;
 }
+
+function getProjectLinkSpanContent(array $project) {
+    $output = '';
+    if (!empty($project['repo_link'])) {
+        if (!empty($project['use_link'])) {
+           $output .= '<a class="showcaseview" href="' . $project['repo_link'] . '">View Project / </a>' .
+               '<a class="showcaseview" href="' . $project['use_link'] . '"> / Demo</a>';
+           return $output;
+        } else {
+            $output .= '<a class="showcaseview" href="' . $project['repo_link'] . '">View Project</a>';
+        }
+    }
+    if (!empty($project['use_link'])) {
+        $output .= '<a class="showcaseview" href="' . $project['use_link'] . '">View Demo</a>';
+    }
+    return $output;
+}
+
 
 /*
  * Outputs the HTML required to display each entry in the Contact Me section.
